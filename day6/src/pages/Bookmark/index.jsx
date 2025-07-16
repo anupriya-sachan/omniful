11d49eda2569045230919f4fbf4a6e464ccca694
anupriya-sachan <anupriya.sachan@omniful.ai>
2025-07-17 02:12:28 +0530

    Add responsive

--- day6/src/pages/Bookmark/index.jsx
+++ day6/src/pages/Bookmark/index.jsx
@@ -10,7 +10,7 @@ function Bookmark(){
     return(
         <>
         <div className="font-extrabold text-gray-800 p-4 text-3xl w-screen">Your Bookmarks!</div>
-        <div className="grid grid-cols-3 gap-4 justify-center p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols- gap-4 justify-center p-4">
             {bookmarked.map((item,index)=>{
                 return <Card quote={item.quote} author={item.author} key={item.id} id={item.id}/>
             })}
--- day6/src/pages/Quotes/index.jsx
+++ day6/src/pages/Quotes/index.jsx
@@ -7,7 +7,7 @@ function Quotes(){
     //add loading behavior
     return(
         <>
-            <div className="grid grid-cols-3 gap-10 justify-center p-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-center p-4">
                 {quotes.map((item,index)=>{
                         return <Card quote={item.quote} author={item.author} key={item.id} id={item.id}/>
                 })
