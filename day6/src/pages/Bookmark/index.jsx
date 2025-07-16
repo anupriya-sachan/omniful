66c2b61c42462e6ce5ec10ed292da7d932201987
anupriya-sachan <anupriya.sachan@omniful.ai>
2025-07-17 00:53:05 +0530

    Add changes

--- day6/src/pages/Bookmark/index.jsx
+++ day6/src/pages/Bookmark/index.jsx
@@ -9,7 +9,7 @@ function Bookmark(){
     
     return(
         <>
-        <div className="font-extrabold text-gray-800 p-4 text-3xl">Your Bookmarks!</div>
+        <div className="font-extrabold text-gray-800 p-4 text-3xl w-screen">Your Bookmarks!</div>
         <div className="grid grid-cols-3 gap-4 justify-center p-4">
             {bookmarked.map((item,index)=>{
                 return <Card quote={item.quote} author={item.author} key={item.id} id={item.id}/>
